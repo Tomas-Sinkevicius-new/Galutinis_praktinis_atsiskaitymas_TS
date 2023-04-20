68cbe111bcef32d6dfcf4fa79f93a0272b6e070a
Tomas <tom.sinkevicius@gmail.com>
2023-04-20 16:25:49 +0300

    loginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,38 @@
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase/firebase';
+import { toast } from 'react-hot-toast';
+import LoginForm from '../components/auth/LoginForm';
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const navigate = useNavigate();
+
+  function login({ email, password }) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log('user ===', user);
+        //naviguojam i Profile
+        navigate('/profile');
+        toast.success('Login successful');
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('errorMessage ===', errorMessage);
+        toast.error('somethings Worng');
+      });
+  }
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <LoginForm onLogin={login} />
+    </div>
+  );
 }
 
 export default LoginPage;
